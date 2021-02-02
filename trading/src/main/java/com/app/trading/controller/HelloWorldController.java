5952c691d67dd8c8d06940a827f61ba04caf53d3
Dell <Dell@DESKTOP-HAKHPUB.mshome.net>
2021-02-02 21:35:13 +0530

    Trading User Registration and Authentication using Springboot JWT Token

---
+++ trading/src/main/java/com/app/trading/controller/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.app.trading.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * 
+ * @author nilambari.birari
+ *
+ */
+@RestController
+public class HelloWorldController {
+
+	@RequestMapping({ "/hello" })
+	public String firstPage() {
+		return "Hello World";
+	}
+
+}
---
+++ trading/src/test/java/com/app/trading/TradingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.app.trading;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
